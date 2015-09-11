6f12524ecc736cc498f64373029067355fc4088d
Cobi Moos <cobimoos100@gmail.com>
2015-09-11 15:54:20 -0400

    Added Category Model, removed category column from restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911193513) do
+ActiveRecord::Schema.define(version: 20150911195327) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"

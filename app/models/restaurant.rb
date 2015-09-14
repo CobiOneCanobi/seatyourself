a6f977b70b3820b34a7ca0c9e306dbe6b089221f
Cobi Moos <cobimoos100@gmail.com>
2015-09-14 15:38:19 -0400

    Started validating prescence of restaurant values

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,6 @@ class Restaurant < ActiveRecord::Base
   has_many :reservations
   has_many :users, through: :reservations
   belongs_to :category
+
+  validates :name, :hours_of_operation, :price_range, :address, :summary, :phone_num, :presence => true
 end
